68124de0234706d5fec5d3a74ad446b68b207246
therimuthuselvam <theriselvam37@gmail.com>
2022-11-09 01:41:39 +0530

    Create a few components

--- resume-ui/src/app/app.module.ts
+++ resume-ui/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { SkillsComponent } from './skills/skills.component';
+import { KnownLanguagesComponent } from './known-languages/known-languages.component';
+import { EducationDetailsComponent } from './education-details/education-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileDetailsComponent,
+    SkillsComponent,
+    KnownLanguagesComponent,
+    EducationDetailsComponent
   ],
   imports: [
     BrowserModule,
