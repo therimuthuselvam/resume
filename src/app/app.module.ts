b8f4b46d4985e3c9de4f157985174eaa15a40034
therimuthuselvam <theriselvam37@gmail.com>
2022-11-13 18:29:37 +0530

    Created the .git folder once again inside the resume-ui folder

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { SkillsComponent } from './skills/skills.component';
+import { KnownLanguagesComponent } from './known-languages/known-languages.component';
+import { EducationDetailsComponent } from './education-details/education-details.component';
+import { ExperienceDetailsComponent } from './experience-details/experience-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileDetailsComponent,
+    SkillsComponent,
+    KnownLanguagesComponent,
+    EducationDetailsComponent,
+    ExperienceDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/known-languages/known-languages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-known-languages',
+  templateUrl: './known-languages.component.html',
+  styleUrls: ['./known-languages.component.css']
+})
+export class KnownLanguagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
